8c99f3ed3d4502e4628419724b089566933c883d
vagesh10 <vagee.surla4@gmail.com>
2025-07-21 11:56:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+let container=document.getElementById("colorContainer");
+let text=document.getElementById("selectCode");
+
+function firstButton(){
+    container.style.backgroundColor="blue";
+    text.textContent="blue";
+
+}
+
+function secondButton(){
+     container.style.backgroundColor="pink";
+    text.textContent="pink";
+
+
+}
+function thirdButton(){
+    container.style.backgroundColor="red";
+    text.textContent="red";
+
+}
+
+function fourthButton(){
+     container.style.backgroundColor="green";
+    text.textContent="green";
+
+
+}
